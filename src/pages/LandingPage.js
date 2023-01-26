aba36645295d984027be8e51e2f6522b7c682b0a
Kristof <r0696321@student.thomasmore.be>
2023-01-26 19:22:01 +0100

    landing page done

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,7 +7,7 @@ function ScrollDownSection() {
         scrollTo(0, document.body.scrollHeight);
     }
     return (
-        <section id="ScrollDown" className="demo">
+        <section id="ScrollDown" className="demo mt-5 pt-5">
             <button onClick={scroll}><span/>Scroll</button>
         </section>
     );
@@ -41,18 +41,71 @@ export default function LandingPage() {
 
             <header/>
             <div style={{backgroundImage: "url('./images/achtergrond.jpg')", backgroundRepeat:"no-repeat",
-                backgroundSize: "cover" }}>
-                <div className="masthead" style={{height:"100vh"}}>
-                    <div className="masthead-content text-white">
-                        <div style={{width: "100%", marginRight: "auto", marginLeft: "auto",
-                            paddingRight: "1.5rem", paddingLeft: "1.5rem"}}>
+                backgroundSize: "cover", height:"auto" }}>
+                <div className="px-4 py-5  text-center">
+                    <img className="d-block mx-auto mb-4" src="/images/avatar.jpg" alt=""
+                         width="160" height="160"/>
+                        <h1 className="display-5 fw-bold">Centered hero</h1>
+                        <div className="col-lg-6 mx-auto">
+                            <p className="lead mb-4">Quickly design and customize responsive mobile-first sites with
+                                Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass
+                                variables and mixins, responsive grid system, extensive prebuilt components, and
+                                powerful JavaScript plugins.</p>
+                            <ScrollDownSection/>
+                        </div>
+                </div>
+            </div>
+            <div className="b-example-divider"/>
 
-                            <h1 style={{marginBottom: "1.5rem"}}>text</h1>
-                            <p style={{marginBottom: "3rem"}}>We're working hard to finish the development of this site.</p>
+            <div className="container px-4 py-5" id="hanging-icons">
+                <h2 className="pb-2 border-bottom">Hanging icons</h2>
+                <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div className="col d-flex align-items-start">
+                        <div className="icon-square bg-light text-dark flex-shrink-0 me-3">
+                            <svg className="bi" width="1em" height="1em">
+                                <use xlinkHref="#toggles2"></use>
+                            </svg>
+                        </div>
+                        <div>
+                            <h2>Featured title</h2>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                another sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" className="btn btn-primary">
+                                Primary button
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-start">
+                        <div className="icon-square bg-light text-dark flex-shrink-0 me-3">
+                            <svg className="bi" width="1em" height="1em">
+                                <use xlinkHref="#cpu-fill"></use>
+                            </svg>
+                        </div>
+                        <div>
+                            <h2>Featured title</h2>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                another sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" className="btn btn-primary">
+                                Primary button
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-start">
+                        <div className="icon-square bg-light text-dark flex-shrink-0 me-3">
+                            <svg className="bi" width="1em" height="1em">
+                                <use xlinkHref="#tools"></use>
+                            </svg>
+                        </div>
+                        <div>
+                            <h2>Featured title</h2>
+                            <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with
+                                another sentence and probably just keep going until we run out of words.</p>
+                            <a href="#" className="btn btn-primary">
+                                Primary button
+                            </a>
                         </div>
                     </div>
                 </div>
-                <ScrollDownSection/>
             </div>
             </>
     );

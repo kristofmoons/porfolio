1b907e569a1f4149f1efa1c9dd82c97eafdc411d
Kristof <r0696321@student.thomasmore.be>
2023-03-06 21:19:52 +0100

    add toggle Button

--- src/pages/AchievementPage.js
+++ src/pages/AchievementPage.js
@@ -1,17 +1,33 @@
 import Navbar from "../components/Navbar";
 import {Internships} from "../components/Internships";
 import Projects from "../components/Projects";
+import {useState} from "react";
 
 export default function AchievementPage() {
+
+    const [toggle, setToggle] = useState(true)
 
 return (
     <>
         <Navbar/>
 
-        <h1> projects Internships</h1>
+        <div className="switch-holder">
+            <div className="switch-label">
+                <span>Bluetooth</span>
+            </div>
+            <div className="switch-toggle">
+                <input type="checkbox" id="bluetooth"  onChange={() => setToggle(!toggle)}/>
+                    <label htmlFor="bluetooth"/>
+            </div>
+            <div className="switch-label">
+                <span>Bluetooth</span>
+            </div>
+        </div>
 
-        <Projects/>
+        {toggle? <Projects/>
+            :
         <Internships/>
+        }
 
     </>
 );

d33e95dce6c9ec124587c797353910bc3d503729
Kristof <r0696321@student.thomasmore.be>
2023-07-08 21:30:50 +0200

    update togglebar portfolio

--- src/pages/AchievementPage.js
+++ src/pages/AchievementPage.js
@@ -3,14 +3,16 @@ import Internships from "../components/Internships";
 import Projects from "../components/Projects";
 import {useState} from "react";
 
+
 export default function AchievementPage() {
 
     const [toggle, setToggle] = useState(true)
 
     return (
         <>
-            <Navbar/>
-            <div style={{height: "6rem"}}/>
+            <Navbar />
+
+            <div style={{ height: "6rem" }} />
 
             <div className="container">
                 <div className="row align-items-center">
@@ -20,21 +22,21 @@ export default function AchievementPage() {
                             <span>Projecten</span>
                         </div>
                         <div className="switch-toggle">
-                            <input type="checkbox" id="bluetooth" onChange={() => setToggle(!toggle)}/>
-                            <label htmlFor="bluetooth"/>
+                            <input type="checkbox" id="bluetooth" onChange={() => setToggle(!toggle)} />
+                            <label htmlFor="bluetooth" />
                         </div>
                         <div className="switch-label">
                             <span>Ervaringen</span>
                         </div>
                     </div>
 
-                    {toggle ? <Projects/>
-                        :
-                        <Internships/>
-                    }
+                    <div className="col">
+                        {toggle ? <Projects /> : <Internships />}
+                    </div>
 
                 </div>
             </div>
+
 
 
         </>

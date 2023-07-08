1731b2e1e60ad8f1e6e0a0047f659400f8aa9c01
Kristof <r0696321@student.thomasmore.be>
2023-07-08 20:44:30 +0200

    update internships

--- src/components/Internships.js
+++ src/components/Internships.js
@@ -1,75 +1,65 @@
-function ImageLeft() {
-    return (
-        <div className="border px-4 rounded mx-2">
-        <div className="container pt-4">
-            <div className="row align-items-center mb-3">
-                <div className="col-lg-6 order-2 order-lg-1 overflow-auto">
-                    <h2 className="font-weight-light">Stage bij XP DAYS</h2>
-                    <p className="font-italic text-muted mb-4 overflow-auto" style={{ maxHeight: '400px' }}>
-                        Met trots kan ik vertellen dat ik samen met twee andere studenten een applicatie heb herschreven met React en Firebase als backend. Het betreft een app die de organisatie helpt bij het organiseren van conferenties.<br/><br/>
-                        Het was een uitdagend project, maar we hebben ons er volledig op gestort en met succes voltooid. De oude applicatie was niet meer toereikend voor de behoeften van de organisatie en miste essentiële functies. Door het gebruik van React hebben we een moderne en responsieve gebruikersinterface kunnen creëren die aantrekkelijker en gebruiksvriendelijker is dan de vorige versie.<br/><br/>
-                        We hebben ook Firebase gebruikt als backend voor de app, wat ons veel flexibiliteit en schaalbaarheid gaf. Met behulp van Firebase hebben we functies als real-time updates, authenticatie en databasebeheer kunnen integreren in de app.<br/><br/>
-                        Het was een waardevolle ervaring om met andere studenten samen te werken aan een project van deze omvang. We hebben veel geleerd over teamwork, software-ontwikkeling en het belang van goed projectmanagement. Ik ben trots op wat we hebben bereikt en ik kijk ernaar uit om mijn nieuwe kennis en vaardigheden in toekomstige projecten toe te passen.
-                    </p>
-                </div>
-                <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2">
-                    <img src="/images/experiences/Xpdays.jpg" className="img-fluid mb-4 mb-lg-0" alt="Project image" />
-                </div>
-            </div>
-        </div>
-        </div>
-    );
-}
-
 function ImageRight() {
     return (
-        <div className="container py-5">
-            <div className="row align-items-center">
-                <div className="col-lg-5 px-5 mx-auto">
-
-                    <div className="img-fluid mb-4 mb-lg-0">
-                        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                    <img src="/images/projecten/repairaza/1.png" className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="/images/projecten/repairaza/2.png" className="d-block w-100" alt="..."/>
-                                </div>
-                                <div className="carousel-item">
-                                    <img src="/images/projecten/repairaza/3.png" className="d-block w-100" alt="..."/>
-                                </div>
-                            </div>
-                        </div>
+        <div className="border px-4 rounded mx-2 my-3">
+            <div className="container pt-4">
+                <div className="row align-items-center mb-3">
+                    <div className="col-lg-6 order-2 order-lg-1 overflow-auto">
+                        <h2 className="font-weight-light">Stage bij XP DAYS</h2>
+                        <p className="font-italic text-muted overflow-auto" style={{maxHeight: '400px'}}>
+                            Met trots heb ik samengewerkt met twee andere studenten om een
+                            conferentieorganisatie-applicatie te herschrijven met behulp van React en Firebase als
+                            backend. We hebben met succes de oude applicatie vervangen, die niet voldeed aan de
+                            behoeften van de organisatie en essentiële functies miste. Met React hebben we een moderne
+                            en responsieve gebruikersinterface gecreëerd, die aantrekkelijker en gebruiksvriendelijker
+                            is dan voorheen. Met Firebase als backend hebben we flexibiliteit en schaalbaarheid
+                            toegevoegd, inclusief real-time updates, authenticatie en databasebeheer. Deze samenwerking
+                            heeft ons waardevolle ervaring opgeleverd in teamwork, software-ontwikkeling en
+                            projectmanagement. Ik ben trots op wat we hebben bereikt en ik kijk ernaar uit om mijn
+                            nieuwe kennis en vaardigheden in toekomstige projecten toe te passen.
+                        </p>
                     </div>
-
-                </div>
-                <div className="col-lg-6"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
-                    <h2 className="font-weight-light">Repairaza</h2>
-                    <p className="font-italic text-muted mb-4">
-                        Dit project was om ons aan te leren hoe java spring werkt, we mochten zelf een thema kiezen
-                        en moesten dit proberen te realiseren. Omdat men ouders graag naar restaureer programma's op tv
-                        kijken had ik besloten om een site te maken waar u uw oude spullen kon brengen om er nieuw leven
-                        in te blazen. U kunt de site bezoeken maar het kan zijn dat het even nodig zal hebben om op te
-                        starten.
-                    </p>
-
+                    <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+                        <img src="/images/experiences/Xpdays.jpg" className="img-fluid mb-4 mb-lg-0"
+                             alt="Project image"/>
+                    </div>
                 </div>
             </div>
         </div>
     );
 }
+function ImageLeft() {
+    return (
+        <div className="border px-4 rounded mx-2 my-3">
+            <div className="container py-5">
+                <div className="row align-items-center">
+                    <div className="col-lg-5 px-5 mx-auto">
+                        <img src="/images/experiences/peopleware.png" className="img-fluid mb-4 mb-lg-0" alt="Project image" />
+                    </div>
+                    <div className="col-lg-6">
+                        <h2 className="font-weight-light">Stage bij PeopleWare</h2>
+                        <p className="font-italic text-muted">
+                            Ik heb meegewerkt aan het project "PPW Applicants" waarbij we hebben gewerkt aan het vervangen van het gebruik van Google Spreadsheets en Google Drive door een nieuwe applicatie. Dit project heb ik samen met een klasgenoot uitgevoerd, waarbij onze mentor onze code heeft gereviewd. Het was een uitdaging voor mij omdat ik uit mijn comfortzone moest stappen en werken met C#. Ik was voornamelijk bezig met de backend. Deze ervaring heeft mijn vaardigheden verrijkt en waardevolle ervaring opgeleverd voor mijn portfolio.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+
 
 
 export default function Internships() {
     return (<>
             <div className="container">
                 <h1 className="font-weight-light text-center text-lg-left mt-4 mb-0">Ervaringen</h1>
-                <hr className="mt-2 mb-5"/>
+                <hr className="mt-2 mb-3"/>
             </div>
 
             <ImageLeft/>
-            {/*<ImageRight/>*/}
+            <ImageRight/>
         </>
 
     );
